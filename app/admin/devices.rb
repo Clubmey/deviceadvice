fbe4a72be6e30646f089a27d566f4363c5672f48
Raghu Betina <raghu@firstdraft.com>
2019-06-05 03:24:54 +0000

    Whitelist columns in Device for ActiveAdmin

--- app/admin/devices.rb
+++ app/admin/devices.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Device do
+
+ permit_params :description, :photo, :company_id, :category_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
