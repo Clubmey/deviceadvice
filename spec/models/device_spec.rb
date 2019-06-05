97c979980c430dc150627c8d103d83950e6cc5bb
Raghu Betina <raghu@firstdraft.com>
2019-06-05 03:24:34 +0000

    Direct association between categories and devices

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Device, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:category) }
+
     it { should belong_to(:company) }
 
     it { should have_many(:reviews) }
